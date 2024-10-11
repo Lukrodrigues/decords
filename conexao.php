ac1997bc07dac8ff246b45df03a5ec0a0d69cfaa
Luciano Rodrigues <lukrodrigues68@hotmail.comclear$ git config --global user.email lukrodrigues68@hotmail.comexitcdexit>
2024-10-11 12:02:02 -0300

    nova implementaçao

--- conexao.php
+++ conexao.php
@@ -1,30 +1,18 @@
 <?php
-/*header('Content-Type: text/html; charset=utf-8');
-$conectar = mysqli_connect("localhost",'root',"") or die("Erro na conexao");
-    mysqli_select_db($conectar,"decords") or die ("base não encontrada");
-    mysqli_query($conectar,"SET NAMES 'utf8'");
-    mysqli_query($conectar,'SET character_set_connection=utf8');
-    mysqli_query($conectar,'SET character_set_client=utf8');
-    mysqli_query($conectar,'SET character_set_results=utf8');
-    ini_set('default_charset','UTF-8');
-*/
 
 $servername = "localhost";
+
 $database = "decords";
 $username = "root";
 $password = "";
 //Create connection
-$conn = new mysqli($servername, $database, $username, $password);
-// Check connection
-/*if ($conn) {
-  error_log('Connection failed: ' . mysqli_connect_error());
-}
-echo "Connected successfully";
 
-//mysqli_close($conn);*/
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($servername, $database, $username, $password);
+
+// Check connection
 
 // Verificar conexão
+/*
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);  // Exibe o erro e encerra o script
 }
