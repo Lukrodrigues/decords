deaa4aab2aebeb3410f8396b6b11cdc497d60c43
Luciano Rodrigues <lukrodrigues68@hotmail.comclear$ git config --global user.email lukrodrigues68@hotmail.comexitcdexit>
2024-11-29 22:01:11 -0300

    Ajuste na logica de exercicios

--- exercicio.php
+++ exercicio.php
@@ -4,181 +4,207 @@ header('Content-Type: text/html; charset=utf-8');
 session_start();
 ?>
 <html lang="pt-br">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title>Decords Musica e Teoria</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">  
-		<meta name="description" content="Decords Música e Teoria">
-	    <meta name="" content="Luciano Moraes Rodrigues">
-	    <link rel="icon" href="img/favicon-96x96.png"> 
-	    <link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/bootstrap-theme.min.css" rel="stylesheet"> 
-		<link href="css/theme.css" rel="stylesheet">
-		<script src="js/document.min.js"></script>
-		<script src="js/jquery.min.js"></script>
-	   <script src="js/bootstrap.min.js"></script>
-	
-		<!-- Support partitura -->
-  <script src="js/partitura/vexflow-min.js"></script>
-  <script src="js/partitura/underscore-min.js"></script>
-  <script src="js/partitura/jquery.js"></script>
-  <script src="js/partitura/tabdiv-min.js"></script>
-		<!-- Support partitura -->
 
-<style>
-#continua{display:none;}
-</style>
-		
-	<script type="text/javascript">	
-function escolha(){
-  var nnn = getRadioValor('res');
-  document.getElementById('inf').value = nnn; 
- //alert("ola"); 
- }
- function getRadioValor(name){
-  var rads = document.getElementsByName(name);
-  
-  for(var i = 0; i < rads.length; i++){
-   if(rads[i].checked){
-    return rads[i].value;
-   }
-  
-  }
-  
-  return null;
- }
- function finaliza() {
-  $.ajax({
-    type: "POST",
-    url: "valida_exercicio.php",
-    data: {
-      escolha: $('#inf').val(),
-	  resposta: $('#resp').val(),
-	  cod: $('#usr').val(),
-	  exe: $('#exe').val()
-    },
-    success: function(data) {
-      $('#resolucao').html(data);
-	 termina();
-    }
-  });
-}
-function termina()
-{
-$('#questao').hide();
-//$('#continua').show();
-}
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Decords Musica e Teoria</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Decords Música e Teoria">
+	<meta name="" content="Luciano Moraes Rodrigues">
+	<link rel="icon" href="img/favicon-96x96.png">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap-theme.min.css" rel="stylesheet">
+	<link href="css/theme.css" rel="stylesheet">
+	<script src="js/document.min.js"></script>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
 
-			
-</script>	
+	<!-- Support partitura -->
+	<script src="js/partitura/vexflow-min.js"></script>
+	<script src="js/partitura/underscore-min.js"></script>
+	<script src="js/partitura/jquery.js"></script>
+	<script src="js/partitura/tabdiv-min.js"></script>
+	<!-- Support partitura -->
 
+	<style>
+		#continua {
+			display: none;
+		}
+	</style>
+
+	<script type="text/javascript">
+		function escolha() {
+			var nnn = getRadioValor('res');
+			document.getElementById('inf').value = nnn;
+			//alert("ola"); 
+		}
+
+		function getRadioValor(name) {
+			var rads = document.getElementsByName(name);
+
+			for (var i = 0; i < rads.length; i++) {
+				if (rads[i].checked) {
+					return rads[i].value;
+				}
+
+			}
+
+			return null;
+		}
+
+		function finaliza() {
+			$.ajax({
+				type: "POST",
+				url: "valida_exercicio.php",
+				data: {
+					escolha: $('#inf').val(),
+					resposta: $('#resp').val(),
+					cod: $('#usr').val(),
+					exe: $('#exe').val()
+				},
+				success: function(data) {
+					$('#resolucao').html(data);
+					termina();
+				}
+			});
+		}
+
+		function termina() {
+			$('#questao').hide();
+			//$('#continua').show();
+		}
+	</script>
+
+</head>
+
+<body role="document">
 	</head>
-		<body role="document">
-		</head>
 
-  <body role="document">
+	<body role="document">
 
-    <!-- Fixed navbar -->
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-		<div class="row">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index.php">Decords</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="iniciantes.php">voltar</a></li>
-			<div class="container theme showcase" role="main">
-			</nav>
-				<div class="page-header">
-					<h1>Exercício:</h1>
+		<!-- Fixed navbar -->
+		<nav class="navbar navbar-inverse navbar-fixed-top">
+			<div class="row">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="index.php">Decords</a>
 				</div>
-				<nav class="navbar navbar-default navbar" role="navigation">
-				<div class="container-fluid">
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="iniciantes.php">voltar</a></li>
+						<div class="container theme showcase" role="main">
+		</nav>
+		<div class="page-header">
+			<h1>Exercício:</h1>
+		</div>
+		<nav class="navbar navbar-default navbar" role="navigation">
+			<div class="container-fluid">
 				<div class="row-fluid">
-				<div class="box-content">
-				<div class="form-horizontal">
-				
-	<body>			
-				<?php
-				//conclusao do exercicio
-				 echo '<div id="resolucao"></div>';	
-				
-//id do exercicio	
-header('Content-Type: text/html; charset=utf-8');			
-$cod= $_GET['id'];
-$codd=$_SESSION['AlunoId'];
-include_once("conexao.php");
+					<div class="box-content">
+						<div class="form-horizontal">
 
-						$sql = "SELECT * FROM exercicios WHERE id='$cod'";
-						$queryResult = mysql_query($sql) or die(mysql_error());
-						while ($row = mysql_fetch_array($queryResult)) {
-						$pergunta = $row['pergunta'];
-	     				$tablatura = $row['tablatura'];
-						$dica = $row['dica'];
-						$a = $row['a'];
-						$b = $row['b'];
-						$c = $row['c'];
-						$d = $row['d'];
-						$resp = $row['resposta'];
-							echo'<div id="questao">
-							<div class="form-group" >
-                                            <label>Enunciado</label>
-                                            <textarea class="form-control" rows="3">'.$pergunta.'</textarea>
-											<div class="vex-tabdiv" width=500 scale=1.0 editor="false" editor_height=100>
-											'.$tablatura.'
-											</div> 
-                                        </div>
-									      <div class="form-group">
-                                            <label>Opções</label>
-                                            <div class="radio">
-                                                <label>
-                                                    <input type="radio" name="res" id="a" value="a" onclick="escolha()">a - '.$a.'
-                                                </label>
-                                            </div>
-                                            <div class="radio">
-                                                <label>
-                                                    <input type="radio" name="res" id="b" value="b" onclick="escolha()">b - '.$b.'
-                                                </label>
-                                            </div>
-                                            <div class="radio">
-                                                <label>
-                                                    <input type="radio" name="res" id="c" value="c" onclick="escolha()">c - '.$c.'
-                                                </label>
-                                            </div>
-											<div class="radio">
-                                                <label>
-                                                    <input type="radio" name="res" id="d" value="d" onclick="escolha()">d - '.$d.'
-                                                </label>
-                                            </div>
-											<div class="form-actions"></br>
-											<button id="envio" type="submit" class="btn btn-primary" onclick="finaliza()">Responder</button>
-											<button class="btn btn-danger">Dica:'.$dica.'</button>
-										
-											</div>
-									  </div>
-									</div>  
-									  <input TYPE="hidden" name="inf" id="inf" value="nulo">
-							          <input TYPE="hidden" name="resp" id="resp" value="'.$resp.'">
-									  <input TYPE="hidden" name="usr" id="usr" value="'.$codd.'">
-									  <input TYPE="hidden" name="exe" id="exe" value="'.$cod.'">
-									  
-									  ';
-									  mysql_query("SET NAMES 'utf-8'");
-									  mysql_query("SET character_set_connection=utf-8");
-									  mysql_query("SET character_set_clent=utf-8");
-									  mysql_query("SET character_set_results=utf-8");
-						}
-						
-							?>
-					
-			</div>
+							<body>
+								<?php
+								// Conclusão do exercício
+								echo '<div id="resolucao"></div>';
+
+								// Configurações iniciais
+								header('Content-Type: text/html; charset=utf-8');
+								if (session_status() === PHP_SESSION_NONE) {
+									session_start();
+								}
+								$cod = isset($_GET['id']) ? intval($_GET['id']) : 0; // Sanitiza o ID do exercício
+								$codd = isset($_SESSION['AlunoId']) ? intval($_SESSION['AlunoId']) : 0;
+
+								// Conexão com o banco de dados
+								include_once("conexao.php");
+
+								// Verifica se a conexão foi estabelecida corretamente
+								if ($conn->connect_error) {
+									die("Falha na conexão: " . $conn->connect_error);
+								}
+
+								// Define o conjunto de caracteres para UTF-8
+								$conn->set_charset("utf8");
+
+								// Consulta SQL
+								$sql = "SELECT * FROM exercicios WHERE id = ?";
+								$stmt = $conn->prepare($sql);
+								$stmt->bind_param("i", $cod);
+								$stmt->execute();
+								$result = $stmt->get_result();
+
+								if ($result->num_rows > 0) {
+									while ($row = $result->fetch_assoc()) {
+										$pergunta = $row['pergunta'];
+										$tablatura = $row['tablatura'];
+										$dica = $row['dica'];
+										$a = $row['a'];
+										$b = $row['b'];
+										$c = $row['c'];
+										$d = $row['d'];
+										$resp = $row['resposta'];
+
+										echo '
+        <div id="questao">
+            <div class="form-group">
+                <label>Enunciado</label>
+                <textarea readonly class="form-control" rows="3">' . htmlspecialchars($pergunta, ENT_QUOTES, 'UTF-8') . '</textarea>
+                <div class="vex-tabdiv" width=500 scale=1.0 editor="false" editor_height=100>
+                    ' . htmlspecialchars($tablatura, ENT_QUOTES, 'UTF-8') . '
+                </div> 
+            </div>
+            <div class="form-group">
+                <label>Opções</label>
+                <div class="radio">
+                    <label>
+                        <input type="radio" name="res" id="a" value="a" onclick="escolha()">a - ' . htmlspecialchars($a, ENT_QUOTES, 'UTF-8') . '
+                    </label>
+                </div>
+                <div class="radio">
+                    <label>
+                        <input type="radio" name="res" id="b" value="b" onclick="escolha()">b - ' . htmlspecialchars($b, ENT_QUOTES, 'UTF-8') . '
+                    </label>
+                </div>
+                <div class="radio">
+                    <label>
+                        <input type="radio" name="res" id="c" value="c" onclick="escolha()">c - ' . htmlspecialchars($c, ENT_QUOTES, 'UTF-8') . '
+                    </label>
+                </div>
+                <div class="radio">
+                    <label>
+                        <input type="radio" name="res" id="d" value="d" onclick="escolha()">d - ' . htmlspecialchars($d, ENT_QUOTES, 'UTF-8') . '
+                    </label>
+                </div>
+                <div class="form-actions"></br>
+                    <button id="envio" type="submit" class="btn btn-primary" onclick="finaliza()">Responder</button>
+                    <button class="btn btn-danger">Dica: ' . htmlspecialchars($dica, ENT_QUOTES, 'UTF-8') . '</button>
+                </div>
+            </div>
+        </div>
+        <input type="hidden" name="inf" id="inf" value="nulo">
+        <input type="hidden" name="resp" id="resp" value="' . htmlspecialchars($resp, ENT_QUOTES, 'UTF-8') . '">
+        <input type="hidden" name="usr" id="usr" value="' . htmlspecialchars($codd, ENT_QUOTES, 'UTF-8') . '">
+        <input type="hidden" name="exe" id="exe" value="' . htmlspecialchars($cod, ENT_QUOTES, 'UTF-8') . '">
+        ';
+									}
+								} else {
+									echo "Nenhum exercício encontrado.";
+								}
+
+								// Fecha a conexão
+								$stmt->close();
+								$conn->close();
+								?>
+
+						</div>
 		</nav>
 	</body>
-</html>						
+
+</html>
--- valida_exercicio.php
+++ valida_exercicio.php
@@ -1,187 +1,114 @@
 <?php
+// Verifica se a sessão não foi iniciada
+if (session_status() === PHP_SESSION_NONE) {
+	session_start();
+}
+
 header('Content-Type: text/html; charset=utf-8');
-session_start();
 include_once("conexao.php");
 
+// Valida os parâmetros enviados
+if (!isset($_POST['escolha'], $_POST['resposta'], $_POST['cod'], $_POST['exe'])) {
+	echo "Erro: Parâmetros insuficientes.";
+	exit;
+}
+
+header('Content-Type: text/html; charset=utf-8');
+if (session_status() === PHP_SESSION_NONE) {
+	session_start();
+}
 
 $escolha = $_POST['escolha'];
 $resp = $_POST['resposta'];
-$cod = $_POST['cod']; //aluno
-$exe = $_POST['exe']; //exercicio
-$nivell=$_SESSION['AlunoNivel']; // nivel aluno
-$data=date("Y-m-d H:i:sa");
-$linkk="exercicios.php";
+$cod = $_POST['cod']; // ID do aluno
+$exe = $_POST['exe']; // ID do exercício
+$nivell = $_SESSION['AlunoNivel']; // Nível do aluno
+$data = date("Y-m-d H:i:sa");
+$linkk = "Exercicios.php";
 
+// Conexão com o banco de dados usando MySQLi
+include_once("conexao.php");
 
+// Verifica se a resposta está correta
+if ($escolha == $resp) {
 
- 
-//verifica se resposta esta correta
-if($escolha==$resp){
+	// Definindo a consulta com placeholders (?):
+	$sql5 = "INSERT INTO alunos_exercicios (id_usuario, id_exercicios, data_termino, resultado, status) 
+VALUES (?, ?, ?, ?, ?)";
 
-//REGISTRA A ATIVIDADE COMO EFETUADA E BOTÃO CONTINUAR
-$sql5 = "INSERT INTO alunos_exercicios (id_usuario,id_exercicios,data_termino,resultado,status) VALUES('$cod','$exe','$data','1','1')";
-$queryResult5 = mysql_query($sql5) or die(mysql_error());
+	// Prepara a consulta:
+	if ($stmt5 = $conn->prepare($sql5)) {
+		// Definindo os tipos dos parâmetros: i = inteiro, s = string
+		$stmt5->bind_param("iisss", $cod, $exe, $data, $resultado, $status);
 
-						//quantidade de exercicios feitos
-						$sql4 = "SELECT count(*) FROM alunos_exercicios where id_usuario='$cod' and id_exercicios in (
-						select id from exercicios where nivel='$nivell')";
-						$queryResult4 = mysql_query($sql4) or die(mysql_error());
-						$row4 = mysql_fetch_row($queryResult4);
-						$qtd_feitos = $row4[0];
-						
-						
-						//quantidade de exercicios feitos certos
-						$sql4 = "SELECT count(*) FROM alunos_exercicios where resultado=1 and id_usuario='$cod' and id_exercicios in (
-						select id from exercicios where nivel='$nivell')";
-						$queryResult4 = mysql_query($sql4) or die(mysql_error());
-						$row4 = mysql_fetch_row($queryResult4);
-						$qtd_feitos_certos = $row4[0];
-						
-						//quantidade total de exercicios
-						$sql4 = "select count(*) from exercicios where nivel='$nivell'";
-						$queryResult4 = mysql_query($sql4) or die(mysql_error());
-						$row4 = mysql_fetch_row($queryResult4);
-						$qtd_total = $row4[0];
-							
-//verifica se fez todos os exercicios disponiveis 
-if($qtd_total==$qtd_feitos){
-//calculando porcentagem
-$percent = (($qtd_feitos_certos / $qtd_total) * 100);
+		// Definindo as variáveis
+		$resultado = 1;  // Resposta correta
+		$status = 1;     // Status de exercício concluído
 
-if($percent>=60){
-	$sql6 = "update alunos set nivel=$nivell+1 where id='$cod'";
-    $queryResult6 = mysql_query($sql6) or die(mysql_error());
-	$_SESSION['AlunoNivel']=$nivell+1;
-	$nivell=$_SESSION['AlunoNivel'];
-if($nivell==1){$linkk = "iniciantes.php";}else if ($nivell==2){$linkk = "intermediarios.php";}else if ($nivell==3){$linkk = "avancados.php";}
+		// Executa a consulta
+		$stmt5->execute();
 
-	   
-	   	   	echo'</br></br>
-							<div id="correto" class="alert alert-success">
-							<button type="button" class="close" data-dismiss="alert">×</button>
-							<strong>Correto!</strong> Você concluiu o nivel com mais de 60% de Acertos!
-							</div><br>
-							 <a  type="button" class="btn btn-primary" href="'.$linkk.'" >Continua</a>';
-	
-}else {
-	
-	 					
-	
-	$sql6 = "delete from alunos_exercicios where id_usuario='$cod' and id_exercicios in (
-						select id from exercicios where nivel='$nivell')";
-    $queryResult6 = mysql_query($sql6) or die(mysql_error());
-if($nivell==1){$linkk = "iniciantes.php";}else if ($nivell==2){$linkk = "intermediarios.php";}else if ($nivell==3){$linkk = "avancados.php";}
+		// Verifica se a execução foi bem-sucedida
+		if ($stmt5->affected_rows > 0) {
+			echo "Atividade registrada com sucesso!";
+		} else {
+			echo "Erro ao registrar atividade.";
+		}
 
-	   
-	   	echo'</br></br>
-							<div id="correto" class="alert alert-success">
-							<button type="button" class="close" data-dismiss="alert">×</button>
-							<strong>Correto!</strong> Você não concluiu o minimo de 60%, refazer o nivel!
-							</div><br>
-							 <a  type="button" class="btn btn-primary" href="'.$linkk.'" >Continua</a>';
-	   
+		// Fecha o statement
+		$stmt5->close();
+	} else {
+		die("Erro ao preparar a consulta: " . $conn->error);
+	}
+
+	// Verifica se todos os exercícios do nível atual foram concluídos
+	$sql_check_concluidos = "SELECT COUNT(*) FROM alunos_exercicios 
+                             WHERE id_usuario = ? AND status = 1 
+                             AND id_exercicios IN (SELECT id FROM exercicios WHERE nivel = ?)";
+	if ($stmt_check_concluidos = $conn->prepare($sql_check_concluidos)) {
+		$stmt_check_concluidos->bind_param("ii", $cod, $nivell);
+		$stmt_check_concluidos->execute();
+		$stmt_check_concluidos->bind_result($concluidos);
+		$stmt_check_concluidos->fetch();
+		$stmt_check_concluidos->close();
+	} else {
+		die("Erro ao preparar a consulta: " . $conn->error);
+	}
+
+	// Conta o total de exercícios do nível atual
+	$sql_total_exercicios = "SELECT COUNT(*) FROM exercicios WHERE nivel = ?";
+	if ($stmt_total = $conn->prepare($sql_total_exercicios)) {
+		$stmt_total->bind_param("i", $nivell);
+		$stmt_total->execute();
+		$stmt_total->bind_result($total_exercicios);
+		$stmt_total->fetch();
+		$stmt_total->close();
+	} else {
+		die("Erro ao preparar a consulta: " . $conn->error);
+	}
+
+	// Se todos os exercícios do nível foram concluídos, avança para o próximo nível
+	if ($concluidos == $total_exercicios) {
+		$_SESSION['AlunoNivel'] = $nivell + 1;  // Avança o nível do aluno
+		echo "<div class='alert alert-success'>Parabéns! Você concluiu o nível $nivell. Você foi promovido ao próximo nível!</div>";
+	} else {
+		echo "<div class='alert alert-info'>Você concluiu este exercício! Continue para o próximo.</div>";
+	}
+} else {
+	echo "<div class='alert alert-danger'>Resposta incorreta! A resposta correta é: <strong>$resp</strong></div>";
 }
 
+// Fecha a conexão com o banco de dados
 
+try {
+	// [Seu código para a execução das consultas vai aqui]
 
-}else{
-		if($nivell==1){$linkk = "iniciantes.php";}else if ($nivell==2){$linkk = "intermediarios.php";}else if ($nivell==3){$linkk = "avancados.php";}
-	echo'</br></br>
-							<div id="correto" class="alert alert-success">
-							<button type="button" class="close" data-dismiss="alert">×</button>
-							<strong>Correto!</strong> Muito bem!
-							</div><br>
-							 <a  type="button" class="btn btn-primary" href="'.$linkk.'" >Continua</a>';
-}
-	
-}else{
-
-//REGISTRA A ATIVIDADE COMO EFETUADA
-$sql5 = "INSERT INTO alunos_exercicios (id_usuario,id_exercicios,data_termino,resultado,status) VALUES('$cod','$exe','$data','2','1')";
-$queryResult5 = mysql_query($sql5) or die(mysql_error());
-
-						//quantidade de exercicios feitos
-						$sql4 = "SELECT count(*) FROM alunos_exercicios where id_usuario='$cod' and id_exercicios in (
-						select id from exercicios where nivel='$nivell')";
-						$queryResult4 = mysql_query($sql4) or die(mysql_error());
-						$row4 = mysql_fetch_row($queryResult4);
-						$qtd_feitos = $row4[0];
-						
-						
-						//quantidade de exercicios feitos certos
-						$sql4 = "SELECT count(*) FROM alunos_exercicios where resultado=1 and id_usuario='$cod' and id_exercicios in (
-						select id from exercicios where nivel='$nivell')";
-						$queryResult4 = mysql_query($sql4) or die(mysql_error());
-						$row4 = mysql_fetch_row($queryResult4);
-						$qtd_feitos_certos = $row4[0];
-						
-						//quantidade total de exercicios
-						$sql4 = "select count(*) from exercicios where nivel='$nivell'";
-						$queryResult4 = mysql_query($sql4) or die(mysql_error());
-						$row4 = mysql_fetch_row($queryResult4);
-						$qtd_total = $row4[0];
-
-//verifica se fez todos os exercicios disponiveis 
-
-if($qtd_total==$qtd_feitos){
-//calculando porcentagem
-$percent = (($qtd_feitos_certos / $qtd_total) * 100);
-
-if($percent>=60){
-	$sql6 = "update alunos set nivel=$nivell+1 where id='$cod'";
-    $queryResult6 = mysql_query($sql6) or die(mysql_error());
-	$_SESSION['AlunoNivel']=$nivell+1;
-	$nivell=$_SESSION['AlunoNivel'];
-if($nivell==1){$linkk = "iniciantes.php";}else if ($nivell==2){$linkk = "intermediarios.php";}else if ($nivell==3){$linkk = "avancados.php";}
-	   
-	   echo'</br></br>
-							<div id="correto" class="alert alert-danger">
-							<button type="button" class="close" data-dismiss="alert">×</button>
-							<strong>A resposta esta incorreta e com mais de 60% de Acertos!! A resposta correta é '.$resp.'</strong>
-							</div><br>
-							<a type="button" class="btn btn-primary" href="'.$linkk.'" >Continua</a>';
-	   
-	
-}else {
-	
-	$sql6 = "delete from alunos_exercicios where id_usuario='$cod' and id_exercicios in (
-						select id from exercicios where nivel='$nivell')";
-    $queryResult6 = mysql_query($sql6) or die(mysql_error());
-	
-
-if($nivell==1){$linkk = "iniciantes.php";}else if ($nivell==2){$linkk = "intermediarios.php";}else if ($nivell==3){$linkk = "avancados.php";}
-
-echo'</br></br>
-							<div id="correto" class="alert alert-danger">
-							<button type="button" class="close" data-dismiss="alert">×</button>
-							<strong>A resposta esta incorreta e você não concluiu o minimo de 60%, refazer o nivel!! A resposta correta é '.$resp.'</strong>
-							</div><br>
-							<a type="button" class="btn btn-primary" href="'.$linkk.'" >Continua</a>';
-
+	// Fecha os statements se foram inicializados
+	if (isset($stmt)) $stmt->close();
+	if (isset($stmtCheck)) $stmtCheck->close();
+	if (isset($stmtUpdate)) $stmtUpdate->close();
+} catch (Exception $e) {
+	echo "<div class='alert alert-danger'>Erro: " . $e->getMessage() . "</div>";
 }
 
-
-} else{						
-						
-if($nivell==1){$linkk = "iniciantes.php";}else if ($nivell==2){$linkk = "intermediarios.php";}else if ($nivell==3){$linkk = "avancados.php";}
-echo'</br></br>
-							<div id="correto" class="alert alert-danger">
-							<button type="button" class="close" data-dismiss="alert">×</button>
-							<strong>A resposta esta incorreta!</strong><br>
-							</div><br>
-							<div id="correto" class="alert alert-success"
-							<strong>"A resposta CORRETA e:" '.$resp.'</strong><br>
-							</div><br>
-							<a type="button" class="btn btn-primary" href="'.$linkk.'" >Continua</a>';
-							
- }
- 
-}
-
-						
-
-
-	
-
- 
-?>
+$conn->close();
